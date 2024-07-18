1499a6442417aefc4d2ec234890e8238921b3911
William <41708290+williammcclean@users.noreply.github.com>
2024-07-18 22:28:07 +0100

    Update ClassDojo.py

--- ClassDojo.py
+++ ClassDojo.py
@@ -1,38 +1,18 @@
 #!/usr/bin/env python3
-
-"""
-Download all ClassDojo photos and videos in your timeline.
-by kecebongsoft <--Thank you!
-How it works:
-1. Fetch list of items in the timeline, if there are multiple pages,
-   it will fetch for all pages.
-2. Collect list of URLs for the attachment for each item.
-3. Download the files into a local temporary directory, and also save
-   the timeline activity as a JSON file.
-How to use:
-1. Modify the session cookie in this script, check your session cookie
-   by opening ClassDojo in a browser and copy the following cookies:
-   dojo_log_session_id, dojo_login.sid, and dojo_home_login.sid.
-2. Run this script and wait for it to finish.
-If errors happen:
-1. Ensure your path is correct if you are on Linux.
-2. Make sure "classdojo_output" directory exists in the same folder as this script.
-3. Make sure you have the correct session cookies set in this script.
-4. Make sure you can open the FEED_URL listed in this script from within your browser (assuming you can open the ClassDojo website).
-"""
-
 import requests
 import json
 import os
 import tempfile
 
 FEED_URL = 'https://home.classdojo.com/api/storyFeed?includePrivate=true'
-DESTINATION = tempfile.mkdtemp(dir='classdojo_output')  # Ensure this directory exists in the same place as this script - not needed as it auto created the folder on the root of my user account
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+DESTINATION = os.path.join(SCRIPT_DIR, 'classdojo_output')
+os.makedirs(DESTINATION, exist_ok=True) 
 
 SESSION_COOKIES = {
-    'dojo_log_session_id': 'copy here',
-    'dojo_login.sid': 'copy here',
-    'dojo_home_login.sid': 'copy here',
+    'dojo_log_session_id': 'enter your cookie log session id',
+    'dojo_login.sid': 'enter your cookie login',
+    'dojo_home_login.sid': 'enter your cookie home login',
 }
 
 NOT_BEFORE = '0000-00-00'  # '2020-08-22'
@@ -42,8 +22,7 @@ def get_items(feed_url):
     resp = requests.get(feed_url, cookies=SESSION_COOKIES)
     data = resp.json()
     prev = data.get('_links', {}).get('prev', {}).get('href')
-    
-    print(f"Fetched {len(data['_items'])} items.")
+
     return data['_items'], prev
 
 def get_contents(feed_url):
@@ -52,11 +31,11 @@ def get_contents(feed_url):
     while prev and feed_url != prev:
         prev_urls, prev = get_items(prev)
         items.extend(prev_urls)
-    
+
     # Save the JSON data for later/inspection.
     with open(os.path.join(DESTINATION, 'data.json'), 'w') as fd:
         fd.write(json.dumps(items, indent=4))
-    
+
     contents = []
     total = 0
     for item in items:
@@ -92,7 +71,8 @@ def download_contents(contents, total):
     index = 0
     highest_day = contents[0]['day']
     for entry in contents:
-        description_name = '{}_{}_description.txt'.format(entry['day'], entry['base_name'])
+        description_name = '{}_{}_description.txt'.format(entry['day'],
+                                                          entry['base_name'])
         with open(os.path.join(DESTINATION, description_name), 'wt') as fd:
             fd.write(entry['description'])
         for item in entry['attachments']:
@@ -101,26 +81,21 @@ def download_contents(contents, total):
             if day > highest_day:
                 highest_day = day
             url = item['url']
-            filename = os.path.join(DESTINATION, '{}_{}_{}'.format(entry['day'], entry['base_name'], item['name']))
+            filename = os.path.join(DESTINATION,
+                                    '{}_{}_{}'.format(entry['day'],
+                                                      entry['base_name'],
+                                                      item['name']))
             if os.path.exists(filename):
                 continue
-            print('Downloading {}/{} on {}: {}'.format(index, total, day, item['name']))
-            try:
-                with open(filename, 'wb') as fd:
-                    resp = requests.get(url, cookies=SESSION_COOKIES)
-                    if resp.status_code == 200:
-                        fd.write(resp.content)
-                        print(f"Successfully downloaded: {filename}")
-                    else:
-                        print(f"Failed to download {filename}. Status code: {resp.status_code}")
-            except Exception as e:
-                print(f"Error downloading {filename}: {e}")
+            print('Downloading {}/{} on {}: {}'
+                  .format(index, total, day, item['name']))
+            with open(filename, 'wb') as fd:
+                resp = requests.get(url, cookies=SESSION_COOKIES)
+                fd.write(resp.content)
     print('Last day of data download: {}'.format(highest_day))
     print('Done!')
 
 if __name__ == '__main__':
     print('Starting')
-    if not os.path.exists(DESTINATION):
-        os.makedirs(DESTINATION)
     contents, total = get_contents(FEED_URL)
     download_contents(contents, total)
